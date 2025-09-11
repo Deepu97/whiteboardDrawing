303187b61056c8f7037454deaae5115dcde5680f
Deepu97 <dy775829@gmail.com>
2025-09-12 00:48:19 +0530

    cors resolve

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 4000;
 // Allowed origins
 const allowedOrigins = [
   "http://127.0.0.1:5501",
-  "http://127.0.0.1:5500"
+  "http://127.0.0.1:5500",
+  "https://deepu97.github.io"
 ];
 
 // ✅ Express APIs ke liye CORS
